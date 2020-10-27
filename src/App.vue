54205308ce53403156ee4116014c7783367fa8e4
Ben <38984686+lumber520@users.noreply.github.com>
2020-10-27 16:37:25 +0000

    vue 3 counter

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+
+  <ScoreCounter name="Player 1" />
+<ScoreCounter name="Player 2" />
+</template>
+
+<script setup>
+  import ScoreCounter from "./components/ScoreCounter";
+  export default {
+    components: {ScoreCounter}
+  }
+</script>
+
+<style>
+#app {
+  font-family:  sans-serif;
+  font-size: 3em;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/ScoreCounter.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h2> {{name}}</h2>
+        <p>{{player.score}}</p>
+        <button @click="decrease">
+            Decrease
+        </button>
+        <button @click="increase">
+            Increase
+        </button>
+    </div>
+</template>
+
+<script setup>
+    import {  reactive } from 'vue'
+export default {
+
+    props:{
+        name:String,
+    },
+    inheritAttrs:false
+};
+    export const player = reactive( {
+        score: 0,
+    });
+    export const increase = () =>{
+        player.score++
+    };
+    export  const decrease = () =>{
+        player.score--
+    }
+</script>
+
+
+<style scoped>
+
+</style>
